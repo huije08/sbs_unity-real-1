4e9506b2ea0ac10a00558ac0420a5ee0aa7dedae
huije08 <huijegang@gmail.com>
2025-05-01 17:25:10 +0900

    (2025.4.11) 스크립트 생성 및 구조

---
+++ Game/Assets/Class2th(Script)/Scripts/Creature.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public Creature()
+    {
+        Debug.Log("Creature");
+    }
+}
